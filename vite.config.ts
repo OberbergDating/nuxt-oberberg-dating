a04b4e2029b708f531dab49854e5363f50e676ff
jm@habit-streak.app <support@habit-streak.app>
2025-04-04 21:40:34 +0200

    add vite config

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      }
+    })
+  ]
+})
