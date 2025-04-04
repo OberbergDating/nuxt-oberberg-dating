a13c3a6d69a330172e770b2167591e71dd26c855
jm@habit-streak.app <support@habit-streak.app>
2025-04-04 22:26:04 +0200

    cleanup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: { enabled: true },
-    modules: ['@vite-pwa/nuxt'],
-    pwa: {
-        /* PWA options */
-    },
+    modules: [],
     app: {
         head: {
             link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
