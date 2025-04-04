444ce4c5c80a1b6f485c162fd2a3bd5e7a7af888
jm@habit-streak.app <support@habit-streak.app>
2025-04-04 22:59:26 +0200

    layout footer heart

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon as Fa } from '@fortawesome/vue-fontawesome';
+</script>
+
 <template>
     <section class="bg-blue-500 min-h-screen bg-cover bg-fixed" style="background-image: url('/unsplash/skyline2.jpg')">
         <header class="mx-auto max-w-6xl py-3 flex justify-between">
@@ -8,21 +13,7 @@
                 <NavMobile />
             </section>
         </header>
-        <section v-if="open" class="bg-white block mx-3 my-3 px-3 py-3 md:hidden">
-            <section class="grid grid-cols-6 gap-3">
-                <div class="col-span-6 sm:col-span-3">
-                    <a href="/match" class="btn btn-primary btn-sm w-full">Match</a>
-                </div>
-                <div class="col-span-6 sm:col-span-3">
-                    <a href="/settings" class="btn btn-primary btn-sm w-full">Settings</a>
-                </div>
-                <div class="col-span-6">
-                    <button @click="open = !open" class="w-full btn btn-secondary text-center">
-                        <span>schliesen</span>
-                    </button>
-                </div>
-            </section>
-        </section>
+        <MobileNavigationContent />
         <main class="min-h-screen mx-auto max-w-6xl ">
             <slot></slot>
         </main>
@@ -31,9 +22,11 @@
                 <div class="col-span-8 md:col-span-2">
                     <a href="/impressum">Impressum</a> & <a href="/datenschutz">Datenschutz</a>
                 </div>
-                <div class="col-span-8 md:col-span-2">
+                <div class="col-span-8 md:col-span-2 flex items-center">
                     <span>Made with</span>
-                    <Fa :icon="faHeart" class="w-5" size="2x" color="red" />
+                    <a href="https://admin.oberberg.dating/_/" target="_blank">
+                        <Fa :icon="faHeart" class="w-5 mx-3" size="2x" color="red" />
+                    </a>
                     <span>by <a href="http://www.jmartz.de" target="_blank"
                             rel="noopener noreferrer">Jonathan</a></span>
                 </div>
@@ -45,9 +38,3 @@
         </footer>
     </section>
 </template>
-
-<script setup lang="ts">
-import { useLocalStorage } from '@vueuse/core';
-
-const open = useLocalStorage('nav-open', false, {});
-</script>
